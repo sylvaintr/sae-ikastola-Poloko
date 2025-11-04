8de12acf3e7790aed912271de08b3e6b476d3e5c
Edouard <edouard.clemenceau.pro@gmail.com>
2025-11-04 18:27:42 +0100

    Change default role by parent

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -43,7 +43,7 @@
                                         @if(Auth::user()->roles->count() > 0)
                                             {{ Auth::user()->roles->first()->name }}
                                         @else
-                                            -
+                                            Parent
                                         @endif
                                     </span>
                                 </div>
