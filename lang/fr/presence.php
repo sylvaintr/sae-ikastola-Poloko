677a4748b9508f2b8f0f71a9a52e96808c1dabf7
Edouard <edouard.clemenceau.pro@gmail.com>
2025-11-06 10:11:09 +0100

    Création fichier traduction, mise en place des pages garderie AM/PM et cantine

---
+++ lang/fr/presence.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'cantine' => 'Cantine',
+    'garderie_matin' => 'Garderie matin',
+    'garderie_soir' => 'Garderie soir',
+];
+
+
--- resources/views/presence/index.blade.php
+++ resources/views/presence/index.blade.php
@@ -1,5 +1,20 @@
 <x-app-layout>
     <div class="container py-4">
+        <div class="mb-3">
+            <ul class="nav nav-tabs border-0">
+                <li class="nav-item me-3">
+                    <a class="nav-link active fw-bold text-warning" href="#" aria-current="page" style="border: none;">{{ __('presence.cantine') }}</a>
+                </li>
+                <li class="nav-item me-3">
+                    <a class="nav-link text-secondary" href="#" style="border: none;">{{ __('presence.garderie_matin') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-secondary" href="#" style="border: none;">{{ __('presence.garderie_soir') }}</a>
+                </li>
+            </ul>
+            <div style="height: 3px; background-color: #e48a1f; margin-top: 4px;"></div>
+        </div>
+
         salut
     </div>
 </x-app-layout>
