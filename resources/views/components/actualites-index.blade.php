0daeb9a1b6278d4406467c75ab8942812d99a0f2
Raphaël Audouard <raudouard@iutbayonne.univ-pau.fr>
2025-11-12 12:27:02 +0100

    Ajout de modification et suppression d'actualités

--- resources/views/components/actualites-index.blade.php
+++ resources/views/components/actualites-index.blade.php
@@ -10,15 +10,22 @@
     <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-6 my-4">
         @foreach($actualites as $actu)
             @if (($actu->type == 'Publique')||$permissionActualies)
-            <div class="bg-white rounded-2xl shadow p-4 my-3">
+            <div class="bg-white shadow rounded p-4 my-3">
                 <h2 class="h4 fw-bold mb-4">{{ $actu->titre }}</h2>
                 <p class="text-gray-700 mb-3">{{ $actu->description }}</p>
                 <p class="text-sm text-gray-500">{{ \Carbon\Carbon::parse($actu->dateP)->format('d/m/Y') }}</p>
                 @if($permissionActualies)
                 <p class="text-gray-700 mb-3">Type : {{ $actu->type }}</p>
                 <div>
-                    <a class="btn btn-primary fw-bold" href="{{ route('actualites.edit') }}">Modifier</a>
-                    <a class="btn btn-danger fw-bold" href="{{ route('actualites.delete') }}">Supprimer</a>
+                    <a class="btn btn-primary fw-bold" href="{{ route('actualites.edit', $actu->idActualite) }}">Modifier</a>
+
+                    <form action="{{ route('actualites.delete', $actu->idActualite) }}" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger fw-bold" onclick="return confirm('Supprimer cette actualité ?')">
+                            Supprimer
+                        </button>
+                    </form>
                 </div>
                 @endif
             </div>
