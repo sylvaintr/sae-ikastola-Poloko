ed97882df940967971604936c53035aaceb9f1d9
Edouard <edouard.clemenceau.pro@gmail.com>
2025-12-02 10:26:57 +0100

    Séléction des classes dans les présences

--- lang/fr/presence.php
+++ lang/fr/presence.php
@@ -5,10 +5,15 @@ return [
     'garderie_matin' => 'Garderie matin',
     'garderie_soir' => 'Garderie soir',
     'classe' => 'Classe',
+    'classes' => 'Classes',
     'eleve' => 'Élève',
     'present' => 'Présent',
     'tout_selectionner' => 'Tout sélectionner',
     'rechercher_eleve' => 'Rechercher un élève',
+    'rechercher_classe' => 'Rechercher une classe',
+    'selectionner_classes_hint' => 'Sélectionnez au moins une classe pour afficher les élèves.',
+    'aucun_resultat' => 'Aucun élève ne correspond à la recherche.',
+    'retirer_classe' => 'Retirer la classe',
 ];
 
 
