dbb4aef2af2fdbb9f38e9614669233c9d72f0e40
Edouard <edouard.clemenceau.pro@gmail.com>
2026-01-11 14:19:53 +0100

    Supression fichier inutile

--- tmp-docs.php
+++
@@ -1,6 +0,0 @@
-﻿<?php
-// This temporary debugging script has been disabled to prevent exposure of sensitive data.
-// If you need document data, implement it through a properly authenticated and authorized endpoint.
-
-http_response_code(404);
-exit;
