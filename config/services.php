b8bc7d9c98a27e70e4234966ce8291c087ea6a1d
Edouard <edouard.clemenceau.pro@gmail.com>
2026-01-12 09:36:35 +0100

    Ajout ignor four clé api test

--- config/services.php
+++ config/services.php
@@ -34,6 +34,9 @@ return [
     'recaptcha' => [
         // Clés de test Google reCAPTCHA pour le développement local
         // Ces clés fonctionnent toujours et acceptent n'importe quelle réponse
+        // Note: Ces clés de test sont publiques et documentées par Google, leur présence dans le code est acceptable
+        'test_site_key' => '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
+        'test_secret_key' => '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe',
         'site_key' => env('RECAPTCHA_SITE_KEY') ?: ((env('APP_ENV') === 'local') ? '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI' : null),
         'secret_key' => env('RECAPTCHA_SECRET_KEY') ?: ((env('APP_ENV') === 'local') ? '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe' : null),
         'enabled' => env('RECAPTCHA_ENABLED', true),
