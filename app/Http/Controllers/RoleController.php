5d28927f03298abe03a4e7fec0f5af5677424ec2
sergiovanni <sergiovanni100@gmail.com>
2026-02-13 10:51:09 +0100

    Refactor RoleController and update routes to use constants for ID parameters

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -22,12 +22,5 @@ class RoleController extends Controller
             ->get(['idRole', 'name']);
 
         return response()->json($roles);
-        $request->validate([
-    'realisateurs' => 'required|array|min:1',
-], [
-    'realisateurs.required' => 'Veuillez sélectionner au moins un utilisateur.',
-    'realisateurs.min' => 'Veuillez sélectionner au moins un utilisateur.',
-]);
-
     }
 }
